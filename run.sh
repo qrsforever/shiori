bbd967bfa46a75f4cfa8647fe8bbb9aa9123b681
Li Dong <705723886@qq.com>
2020-12-18 21:34:21 +0800

    save

--- run.sh
+++ run.sh
@@ -2,8 +2,23 @@
 
 # SHIORI_DIR=/blog/source/_shiori SOCKS5_PROXY=127.0.0.1:1080 /data/shiori/shiori serve -p 8698
 
-docker run -dit --name shiori --network host \
+top_dir=`dirname ${BASH_SOURCE[0]}`
+
+dbroot="/blog/source/_shiori"
+image="shiori"
+
+result=$(docker images --format "{{.ID}}" $image)
+
+if [[ x$result == x || x$1 == x1 ]]
+then
+    cd $top_dir
+    docker build --tag $image --file Dockerfile .
+    cd - > /dev/null
+fi
+
+docker run -dit --name shiori --network bridge \
     --restart unless-stopped --env SOCKS5_PROXY=127.0.0.1:1080 \
-    --volume /blog/source/_shiori:/srv/shiori \
-    --entrypoint /bin/sh shiori \
-    -c "shiori serve -p 8698"
+    --publish 8698:8080 \
+    --volume $dbroot:/srv/shiori \
+    --entrypoint /bin/sh $image \
+    -c "shiori serve -p 8080"
