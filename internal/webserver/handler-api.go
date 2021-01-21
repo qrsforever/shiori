bca8dba38e75d0a08eb750770a80fb5691d22754
eye3ai <985612771@qq.com>
2021-01-21 10:38:48 +0800

    mod: add bookmark local server

--- internal/webserver/handler-api.go
+++ internal/webserver/handler-api.go
@@ -239,6 +239,16 @@ func (h *handler) apiInsertBookmark(w http.ResponseWriter, r *http.Request, ps h
 	err = json.NewDecoder(r.Body).Decode(&book)
 	checkError(err)
 
+    // QRS
+    if !strings.HasPrefix(book.URL, "http") {
+        book.URL = "http://127.0.0.1/" + book.URL
+        if !strings.HasSuffix(book.URL, ".html") {
+            book.URL = book.URL + ".html"
+        }
+    }
+
+    fmt.Println("add bookmark url:", book.URL)
+
 	// Create bookmark ID
 	book.ID, err = h.DB.CreateNewID("bookmark")
 	if err != nil {
